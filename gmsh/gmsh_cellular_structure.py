11c415f3afec26cf9b766fd1033a169dac359271
Gabriel Alkuino <107645552+gsalkuin@users.noreply.github.com>
2024-07-04 15:44:54 -0400

    Add files via upload

--- gmsh/gmsh_cellular_structure.py
+++ gmsh/gmsh_cellular_structure.py
@@ -9,7 +9,6 @@ from math import pi, cos, sin
 import gmsh
 import sys
 
-# Before using any functions in the Python API, Gmsh must be initialized:
 gmsh.initialize()
 gmsh.model.add(filename)
 
@@ -17,14 +16,14 @@ gmsh.model.add(filename)
 gmsh.option.setNumber("General.Terminal", 1)
 # Set the tolerance for combining points
 gmsh.option.setNumber("Geometry.Tolerance", 1e-6)
-# Set the target mesh size globally
+
 gmsh.option.setNumber("Geometry.CopyMeshingMethod", 1)
 gmsh.option.setNumber("Mesh.Algorithm", 5)
 gmsh.option.setNumber("Mesh.MeshSizeMin", 0.25)
 gmsh.option.setNumber("Mesh.MeshSizeMax", 0.25)
 
 # Parameters (in Fig. 2c)
-L = 10 # length of the small square
+L = 10 # length of the unit cell
 w = 0.5
 R = (L-w)/2 # hole radius 
 r = 1.25 # magnet radius
@@ -68,9 +67,8 @@ gmsh.model.occ.cut(cell, [(2, mag) for mag in mags], removeObject=True, removeTo
 gmsh.model.occ.synchronize()
 gmsh.model.mesh.generate(2)
 
-
-# #################################################################################################
-# # Physical groups
+#################################################################################################
+# Physical groups
 
 cell_b = [abs(tag) for [dim, tag] in gmsh.model.getBoundary(cell)]
 
@@ -80,8 +78,8 @@ gmsh.model.addPhysicalGroup(1, [abs(curve) for curve in cell_b], name = "elastic
 gmsh.model.addPhysicalGroup(2, [cell[0][1]], name = "Elastic")
 gmsh.model.addPhysicalGroup(2, mags, name = "Magnetic")
 
-# #################################################################################################
-# # Write to file
+#################################################################################################
+# Write to file
 
 gmsh.write(write_path)
 
@@ -91,7 +89,6 @@ gmsh.write(write_path)
 if '-nopopup' not in sys.argv:
     gmsh.fltk.run()
 
-# We can use this to clear all the model data:
 gmsh.clear()
 
 gmsh.finalize()
